fcdca4a69569667f48505f064b9b13329b184d9a
Benny Rahmat <akunbeben@gmail.com>
2021-03-26 13:46:11 +0800

    styles: Change basic template to AdminLTE

--- resources/views/pages/personil/index.blade.php
+++ resources/views/pages/personil/index.blade.php
@@ -1,48 +1,79 @@
-@extends('layouts.admin_layout')
+@extends('layouts.admin')
 @section('content')
-    <main>
-        <div class="container-fluid">
-            <h1 class="mt-4">List</h1>
-            <ol class="breadcrumb mb-4">
-                <li class="breadcrumb-item"><a href="{{route('home')}}">Dashboard</a></li>
-                <li class="breadcrumb-item active">List</li>
-            </ol>
-            <a href="{{route('personil.create')}}" class="btn btn-success">Create</a>
-            <table class="table table-bordered mt-4">
-                <thead class="table-dark">
-                  <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Personil</th>
-                    <th scope="col">Jumlah</th>
-                    <th scope="col">Lokasi Personil</th>
-                    <th scope="col">Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php $no = 1; ?>
-                    @foreach ($personil as $data)
-                  <tr>
-                    <td><?= $no++ ?></td>
-                    <td>{{$data->personel}}</td>
-                    <td>{{$data->jumlah}}</td>
-                    <td>{{$data->lokasi_personil}}</td>
-                    <td>
-                        
-                                <a href="{{ route('personil.edit', $data->id) }}" class="btn btn-primary btn-sm">
-                                    <i class="fa fa-edit"></i>
-                                </a>
-                            
-                                <form action="{{ route('personil.destroy', $data->id) }}" method="post" class="d-inline">
-                                    @csrf
-                                    @method('delete')
-                                    <button class="btn btn-danger btn-sm">
-                                        <i class="fa fa-trash"></i>
-                                    </button>
-                                </form>
-                    </td>
-                  </tr>
-                    @endforeach
-                </tbody>
-              </table>
-    </main>
+<div class="content-header">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-6">
+				<h1 class="m-0 text-dark">Personel</h1>
+			</div><!-- /.col -->
+			<div class="col-sm-6">
+				<ol class="breadcrumb float-sm-right">
+					<li class="breadcrumb-item"><a href="#">Personel</a></li>
+					<li class="breadcrumb-item active">List</li>
+				</ol>
+			</div><!-- /.col -->
+		</div><!-- /.row -->
+	</div><!-- /.container-fluid -->
+</div>
+<!-- /.content-header -->
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h3 class="card-title">List</h3>
+
+          <div class="text-right">
+            <a class="btn btn-primary btn-sm" title="Create" href="{{ route('personil.create') }}">
+              Create
+            </a>     
+          </div>
+        </div>
+        <div class="card-body">
+          <table class="table table-bordered">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Personil</th>
+                <th>Jumlah</th>
+                <th>Lokasi Personil</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              @if($personil->count() <= 0)
+              <tr>
+                <td colspan="5" class="text-center">There is no data</td>
+              </tr>
+              @endif
+
+              @foreach ($personil as $data)
+              <tr>
+                <td>{{ $loop->iteration }}</td>
+                <td>{{ $data->personel }}</td>
+                <td>{{ $data->jumlah }}</td>
+                <td>{{ $data->lokasi_personil }}</td>
+                <td class="text-center">
+                  <a href="{{ route('personil.edit', $data->id) }}" class="btn btn-primary btn-sm">
+                    <i class="fa fa-edit"></i>
+                  </a>
+                  <form action="{{ route('personil.destroy', $data->id) }}" method="post" class="d-inline">
+                    @csrf
+                    @method('delete')
+                    <button class="btn btn-danger btn-sm">
+                      <i class="fa fa-trash"></i>
+                    </button>
+                  </form>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 @endsection
